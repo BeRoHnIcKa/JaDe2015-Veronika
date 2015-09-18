8b7b153a44dfca46d88385a1afe885ededa9ec3c
Veronika <berohnicka@gmail.com>
2015-09-18 13:46:45 +0300

    Init

---
+++ test/m00/SearchTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m00;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Veronika
+ */
+public class SearchTest {
+    
+    @Test
+    public void testSomeMethod() {
+        
+        fail("The test case is a prototype.");
+    }
+    
+}
